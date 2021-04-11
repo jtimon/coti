630375432c572036de4916f1c397784e4fc80a8d
Jorge Timón <jtimon@jtimon.cc>
2021-04-11 05:10:19 +0200

    Introduce ui::input_string function

--- src/ui.rs
+++ src/ui.rs
@@ -30,3 +30,25 @@ pub fn input_u32(input_min: u32, input_max: u32) -> u32 {
         return input_value;
     }
 }
+
+/// Asks the user for a String between input_min and input_max in length and returns it.
+pub fn input_string(input_min: usize, input_max: usize) -> String {
+
+    let mut input_value;
+    loop {
+        input_value = String::new();
+        io::stdin()
+            .read_line(&mut input_value)
+            .expect("get_input: Failed to read line");
+
+        if input_value.len() < input_min {
+            println!("Min input length {}", input_min);
+            continue;
+        }
+        if input_value.len() > input_max {
+            println!("Max input length {}", input_max);
+            continue;
+        }
+        return input_value;
+    }
+}
