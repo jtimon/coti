a2a3d16839cd1b1b2a93db9687c3dba70058de24
Jorge Timón <jtimon@jtimon.cc>
2021-08-05 04:24:41 +0200

    wip crypto stuff

--- src/crypto.rs
+++ src/crypto.rs
@@ -22,6 +22,14 @@ pub struct EncryptedMsg {
     msg: String,
 }
 
+pub struct SignedEncryptedMsg {
+    algo: CryptoAlgorithm,
+    msg: String,
+    sig: Vec<u8>,
+    signer: PubKey,
+    destination: PubKey,
+}
+
 #[derive(Debug)]
 pub struct DecryptedMsg {
     success: bool,
@@ -77,6 +85,37 @@ impl PubKey {
             CryptoAlgorithm::DUMMY => {
                 // TODO DUMMY the encryption could be just XOR with the pubkey.
                 EncryptedMsg{msg, destination: self.clone()}
+            },
+            CryptoAlgorithm::SECP256K1 => {
+                unimplemented!("TODO ? Nobody cares about SECP256K1 anymore anyway");
+            },
+            CryptoAlgorithm::CURVE25519 => {
+                unimplemented!("TODO ? Nobody cares about CURVE25519 anymore anyway");
+            },
+            CryptoAlgorithm::RSA => {
+                unimplemented!("TODO ? Nobody cares about RSA anymore anyway");
+            },
+            CryptoAlgorithm::EXPERIMENTAL => {
+                unimplemented!("TODO ? Nobody cares about new cryptographic algorithms anymore anyway");
+            }
+        }
+    }
+
+    pub fn encrypt_signed(&self, msg_signed: SignedMsg, signer: PubKey) -> SignedEncryptedMsg {
+        assert!(self.algo == msg_signed.algo);
+
+        match self.algo {
+            CryptoAlgorithm::DUMMY => {
+                // TODO DUMMY the encryption could be just XOR with the pubkey.
+                let enc_msg = self.encrypt(msg_signed.msg);
+
+                SignedEncryptedMsg{
+                    algo: self.algo,
+                    sig: msg_signed.sig,
+                    signer,
+                    destination: enc_msg.destination,
+                    msg: enc_msg.msg,
+                }
             },
             CryptoAlgorithm::SECP256K1 => {
                 unimplemented!("TODO ? Nobody cares about SECP256K1 anymore anyway");
@@ -166,5 +205,22 @@ impl PrivKey {
         }
         // TODO use real cryptography
         DecryptedMsg{msg: encryptedmsg.msg, success: true}
+    }
+
+    pub fn decrypt_verify(&self, signed_encrypted_msg: SignedEncryptedMsg) -> DecryptedMsg {
+        assert!(self.algo == signed_encrypted_msg.algo);
+
+        if signed_encrypted_msg.destination != self.get_pub() {
+            return DecryptedMsg{msg: "Can't decrypt with this private key".to_string(), success: false}
+        }
+
+        // TODO use real cryptography
+        let msg_dec = signed_encrypted_msg.msg;
+        let signedmsg = SignedMsg{msg: msg_dec.clone(), algo: signed_encrypted_msg.algo, sig: signed_encrypted_msg.sig};
+        if !signed_encrypted_msg.signer.verify(&signedmsg) {
+            return DecryptedMsg{msg: "The signature doesn't match the message and pubkey".to_string(), success: false}
+        }
+
+        DecryptedMsg{msg: msg_dec, success: true}
     }
 }
